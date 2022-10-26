9ce404e300286e899538aaac19ade1e885ac4410
[AlenBiz] <[jobbizyaev@gmail.com]>
2022-10-26 17:42:04 +0200

    added a inde

---
+++ inde.js
@@ -0,0 +1,13 @@
+let x = 5;
+
+console.log(++x); // => 6
+console.log(x);   // => 6
+
+console.log(x++); // => 6
+console.log(x);   // => 7
+
+console.log(x--); // => 7
+console.log(x); // => 6
+
+console.log(--x); // => 5
+console.log(x);  // => 5
