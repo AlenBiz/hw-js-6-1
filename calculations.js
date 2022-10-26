af34514aee5416d40cca59164abc6e25462ea175
[AlenBiz] <[jobbizyaev@gmail.com]>
2022-10-26 11:44:25 +0200

    added a assignment operators

--- calculations.js
+++ calculations.js
@@ -1,23 +1,23 @@
 function increaseBalance(x, y) {
-  x = x + y;
+  x += y;
   return x;
 }
 
 
 function decreaseBalance(x,y) {
-x = x - y;
+x -= y;
 return x;
 }
 
 
 function divideBalanceByAccounts(x, y) {
-  x = x / y;
+  x /= y;
   return x;
 }
 
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
+  x %= y;
   return x;
 }
 
